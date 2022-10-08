ef5942b6263526ff03126514bedece235dcfb02f
StepMor <morz21st@gmail.com>
2022-10-08 18:00:48 +0300

    Added camera following the player

---
+++ Assets/Scripts/Camera/PlayerTargeting.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerTargeting : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _smoothTime = 0.1f;
+
+    private Vector3 _velocity;
+    private float _distance = 5f;
+
+    private void Update()
+    {
+        Vector3 targetPosition = new Vector3(_player.position.x, _player.position.y, _distance);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, _smoothTime);
+    }
+}
