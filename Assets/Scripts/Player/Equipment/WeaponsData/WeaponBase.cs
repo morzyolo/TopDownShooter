ba675a4f6382229a5253a8deb0ccdea12b5e283c
StepMor <morz21st@gmail.com>
2022-10-18 17:44:20 +0300

    Added pistol bullet sprite

--- Assets/Scripts/Player/Equipment/WeaponsData/WeaponBase.cs
+++ Assets/Scripts/Player/Equipment/WeaponsData/WeaponBase.cs
@@ -5,9 +5,11 @@ public class WeaponBase : ScriptableObject
 {
     [SerializeField] private string _name;
     [SerializeField] private int _damage;
-    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Sprite _weaponSprite;
+    [SerializeField] private Sprite _bulletSprite;
 
     public string Name { get => _name; }
     public int Damage { get => _damage; }
-    public SpriteRenderer Sprite { get => _sprite; }
+    public Sprite WeaponSprite { get => _weaponSprite; }
+    public Sprite BulletSprite { get => _bulletSprite; }
 }
